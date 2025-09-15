02b23bdc88b89b02e5ea8713539fb3a13ee898c1
PedroAlves7 <139924833+PedroAlves7@users.noreply.github.com>
2025-09-15 14:34:27 -0300

    Update torch_utils.py
    Signed-off-by: PedroAlves7 <139924833+PedroAlves7@users.noreply.github.com>

--- ultralytics/utils/torch_utils.py
+++ ultralytics/utils/torch_utils.py
@@ -159,86 +159,86 @@ def select_device(device="", batch=0, newline=False, verbose=True):
     Notes:
         Sets the 'CUDA_VISIBLE_DEVICES' environment variable for specifying which GPUs to use.
     """
-    if isinstance(device, torch.device) or str(device).startswith(("tpu", "intel")):
-        return device
-
-    s = f"Ultralytics {__version__} 🚀 Python-{PYTHON_VERSION} torch-{TORCH_VERSION} "
-    device = str(device).lower()
-    for remove in "cuda:", "none", "(", ")", "[", "]", "'", " ":
-        device = device.replace(remove, "")  # to string, 'cuda:0' -> '0' and '(0, 1)' -> '0,1'
-
-    # Auto-select GPUs
-    if "-1" in device:
-        from ultralytics.utils.autodevice import GPUInfo
-
-        # Replace each -1 with a selected GPU or remove it
-        parts = device.split(",")
-        selected = GPUInfo().select_idle_gpu(count=parts.count("-1"), min_memory_fraction=0.2)
-        for i in range(len(parts)):
-            if parts[i] == "-1":
-                parts[i] = str(selected.pop(0)) if selected else ""
-        device = ",".join(p for p in parts if p)
-
-    cpu = device == "cpu"
-    mps = device in {"mps", "mps:0"}  # Apple Metal Performance Shaders (MPS)
-    if cpu or mps:
-        os.environ["CUDA_VISIBLE_DEVICES"] = "-1"  # force torch.cuda.is_available() = False
-    elif device:  # non-cpu device requested
-        if device == "cuda":
-            device = "0"
-        if "," in device:
-            device = ",".join([x for x in device.split(",") if x])  # remove sequential commas, i.e. "0,,1" -> "0,1"
-        visible = os.environ.get("CUDA_VISIBLE_DEVICES", None)
-        os.environ["CUDA_VISIBLE_DEVICES"] = device  # set environment variable - must be before assert is_available()
-        if not (torch.cuda.is_available() and torch.cuda.device_count() >= len(device.split(","))):
-            LOGGER.info(s)
-            install = (
-                "See https://pytorch.org/get-started/locally/ for up-to-date torch install instructions if no "
-                "CUDA devices are seen by torch.\n"
-                if torch.cuda.device_count() == 0
-                else ""
-            )
-            raise ValueError(
-                f"Invalid CUDA 'device={device}' requested."
-                f" Use 'device=cpu' or pass valid CUDA device(s) if available,"
-                f" i.e. 'device=0' or 'device=0,1,2,3' for Multi-GPU.\n"
-                f"\ntorch.cuda.is_available(): {torch.cuda.is_available()}"
-                f"\ntorch.cuda.device_count(): {torch.cuda.device_count()}"
-                f"\nos.environ['CUDA_VISIBLE_DEVICES']: {visible}\n"
-                f"{install}"
-            )
-
-    if not cpu and not mps and torch.cuda.is_available():  # prefer GPU if available
-        devices = device.split(",") if device else "0"  # i.e. "0,1" -> ["0", "1"]
-        n = len(devices)  # device count
-        if n > 1:  # multi-GPU
-            if batch < 1:
-                raise ValueError(
-                    "AutoBatch with batch<1 not supported for Multi-GPU training, "
-                    f"please specify a valid batch size multiple of GPU count {n}, i.e. batch={n * 8}."
-                )
-            if batch >= 0 and batch % n != 0:  # check batch_size is divisible by device_count
-                raise ValueError(
-                    f"'batch={batch}' must be a multiple of GPU count {n}. Try 'batch={batch // n * n}' or "
-                    f"'batch={batch // n * n + n}', the nearest batch sizes evenly divisible by {n}."
-                )
-        space = " " * len(s)
-        for i, d in enumerate(devices):
-            s += f"{'' if i == 0 else space}CUDA:{d} ({get_gpu_info(i)})\n"  # bytes to MB
-        arg = "cuda:0"
-    elif mps and TORCH_2_0 and torch.backends.mps.is_available():
-        # Prefer MPS if available
-        s += f"MPS ({get_cpu_info()})\n"
-        arg = "mps"
-    else:  # revert to CPU
-        s += f"CPU ({get_cpu_info()})\n"
-        arg = "cpu"
-
-    if arg in {"cpu", "mps"}:
-        torch.set_num_threads(NUM_THREADS)  # reset OMP_NUM_THREADS for cpu training
-    if verbose:
-        LOGGER.info(s if newline else s.rstrip())
-    return torch.device(arg)
+    # if isinstance(device, torch.device) or str(device).startswith(("tpu", "intel")):
+    #     return device
+    #
+    # s = f"Ultralytics {__version__} 🚀 Python-{PYTHON_VERSION} torch-{TORCH_VERSION} "
+    # device = str(device).lower()
+    # for remove in "cuda:", "none", "(", ")", "[", "]", "'", " ":
+    #     device = device.replace(remove, "")  # to string, 'cuda:0' -> '0' and '(0, 1)' -> '0,1'
+    #
+    # # Auto-select GPUs
+    # if "-1" in device:
+    #     from ultralytics.utils.autodevice import GPUInfo
+    #
+    #     # Replace each -1 with a selected GPU or remove it
+    #     parts = device.split(",")
+    #     selected = GPUInfo().select_idle_gpu(count=parts.count("-1"), min_memory_fraction=0.2)
+    #     for i in range(len(parts)):
+    #         if parts[i] == "-1":
+    #             parts[i] = str(selected.pop(0)) if selected else ""
+    #     device = ",".join(p for p in parts if p)
+    #
+    # cpu = device == "cpu"
+    # mps = device in {"mps", "mps:0"}  # Apple Metal Performance Shaders (MPS)
+    # if cpu or mps:
+    #     os.environ["CUDA_VISIBLE_DEVICES"] = "-1"  # force torch.cuda.is_available() = False
+    # elif device:  # non-cpu device requested
+    #     if device == "cuda":
+    #         device = "0"
+    #     if "," in device:
+    #         device = ",".join([x for x in device.split(",") if x])  # remove sequential commas, i.e. "0,,1" -> "0,1"
+    #     visible = os.environ.get("CUDA_VISIBLE_DEVICES", None)
+    #     os.environ["CUDA_VISIBLE_DEVICES"] = device  # set environment variable - must be before assert is_available()
+    #     if not (torch.cuda.is_available() and torch.cuda.device_count() >= len(device.split(","))):
+    #         LOGGER.info(s)
+    #         install = (
+    #             "See https://pytorch.org/get-started/locally/ for up-to-date torch install instructions if no "
+    #             "CUDA devices are seen by torch.\n"
+    #             if torch.cuda.device_count() == 0
+    #             else ""
+    #         )
+    #         raise ValueError(
+    #             f"Invalid CUDA 'device={device}' requested."
+    #             f" Use 'device=cpu' or pass valid CUDA device(s) if available,"
+    #             f" i.e. 'device=0' or 'device=0,1,2,3' for Multi-GPU.\n"
+    #             f"\ntorch.cuda.is_available(): {torch.cuda.is_available()}"
+    #             f"\ntorch.cuda.device_count(): {torch.cuda.device_count()}"
+    #             f"\nos.environ['CUDA_VISIBLE_DEVICES']: {visible}\n"
+    #             f"{install}"
+    #         )
+    #
+    # if not cpu and not mps and torch.cuda.is_available():  # prefer GPU if available
+    #     devices = device.split(",") if device else "0"  # i.e. "0,1" -> ["0", "1"]
+    #     n = len(devices)  # device count
+    #     if n > 1:  # multi-GPU
+    #         if batch < 1:
+    #             raise ValueError(
+    #                 "AutoBatch with batch<1 not supported for Multi-GPU training, "
+    #                 f"please specify a valid batch size multiple of GPU count {n}, i.e. batch={n * 8}."
+    #             )
+    #         if batch >= 0 and batch % n != 0:  # check batch_size is divisible by device_count
+    #             raise ValueError(
+    #                 f"'batch={batch}' must be a multiple of GPU count {n}. Try 'batch={batch // n * n}' or "
+    #                 f"'batch={batch // n * n + n}', the nearest batch sizes evenly divisible by {n}."
+    #             )
+    #     space = " " * (len(s) + 1)
+    #     for i, d in enumerate(devices):
+    #         s += f"{'' if i == 0 else space}CUDA:{d} ({get_gpu_info(i)})\n"  # bytes to MB
+    #     arg = "cuda:0"
+    # elif mps and TORCH_2_0 and torch.backends.mps.is_available():
+    #     # Prefer MPS if available
+    #     s += f"MPS ({get_cpu_info()})\n"
+    #     arg = "mps"
+    # else:  # revert to CPU
+    #     s += f"CPU ({get_cpu_info()})\n"
+    #     arg = "cpu"
+    #
+    # if arg in {"cpu", "mps"}:
+    #     torch.set_num_threads(NUM_THREADS)  # reset OMP_NUM_THREADS for cpu training
+    # if verbose:
+    #     LOGGER.info(s if newline else s.rstrip())
+    return torch.device("xpu")
 
 
 def time_sync():
@@ -957,6 +957,53 @@ class EarlyStopping:
                 f"i.e. `patience=300` or use `patience=0` to disable EarlyStopping."
             )
         return stop
+
+
+class FXModel(nn.Module):
+    """
+    A custom model class for torch.fx compatibility.
+
+    This class extends `torch.nn.Module` and is designed to ensure compatibility with torch.fx for tracing and graph
+    manipulation. It copies attributes from an existing model and explicitly sets the model attribute to ensure proper
+    copying.
+
+    Attributes:
+        model (nn.Module): The original model's layers.
+    """
+
+    def __init__(self, model):
+        """
+        Initialize the FXModel.
+
+        Args:
+            model (nn.Module): The original model to wrap for torch.fx compatibility.
+        """
+        super().__init__()
+        copy_attr(self, model)
+        # Explicitly set `model` since `copy_attr` somehow does not copy it.
+        self.model = model.model
+
+    def forward(self, x):
+        """
+        Forward pass through the model.
+
+        This method performs the forward pass through the model, handling the dependencies between layers and saving
+        intermediate outputs.
+
+        Args:
+            x (torch.Tensor): The input tensor to the model.
+
+        Returns:
+            (torch.Tensor): The output tensor from the model.
+        """
+        y = []  # outputs
+        for m in self.model:
+            if m.f != -1:  # if not from previous layer
+                # from earlier layers
+                x = y[m.f] if isinstance(m.f, int) else [x if j == -1 else y[j] for j in m.f]
+            x = m(x)  # run
+            y.append(x)  # save output
+        return x
 
 
 def attempt_compile(
